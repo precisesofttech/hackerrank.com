87d293e76909259692411b415aacc9ae09533746
samrat1010 <60088365+samrat1010@users.noreply.github.com>
2020-02-15 11:55:11 +0530

    Update kangaroo.js

--- implementation algos/kangaroo.js
+++ implementation algos/kangaroo.js
@@ -1,3 +1,17 @@
+/*Kangaroo
+
+Complete the function kangaroo in the editor below. It should return YES if they reach the same position at the same time, or NO if they don't.
+
+kangaroo has the following parameter(s):
+
+x1, v1: integers, starting position and jump distance for kangaroo 1
+x2, v2: integers, starting position and jump distance for kangaroo 2
+
+Input Format
+
+A single line of four space-separated integers denoting the respective values of x1,v1 ,x2 and v2.
+*/
+
 function kangaroo(x1, v1, x2, v2) {
 var x = x2 - x1;
 var y = v1 - v2;
@@ -12,3 +26,4 @@ var y = v1 - v2;
    }
 }
 console.log(kangaroo(0, 3, 4, 2));
+
