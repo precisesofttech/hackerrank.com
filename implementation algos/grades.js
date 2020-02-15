a4c5d36de80edb608dba61a215b4a30018a693f2
samrat1010 <60088365+samrat1010@users.noreply.github.com>
2020-02-15 11:54:20 +0530

    Update grades.js

--- implementation algos/grades.js
+++ implementation algos/grades.js
@@ -1,3 +1,16 @@
+/* Complete the function gradingStudents in the editor below. It should return an integer array consisting of rounded grades.
+
+gradingStudents has the following parameter(s):
+
+Student 1 received a 73, and the next multiple of 5 from 73 is 75. Since 75-73<3 , the student's grade is rounded to 75.
+Student 2 received a 67, and the next multiple of 5 from 67 is 70. Since 70-67=3, the grade will not be modified and the student's final grade is 67.
+Student 3 received a 38, and the next multiple of 5 from 38 is 40. Since 40-38<3, the student's grade will be rounded to 40.
+Student 4 received a grade below 38, so the grade will not be modified and the student's final grade is 33.
+
+*/
+
+
+
 
 function gradingStudents(grades) {
   
